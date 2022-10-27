65a52ade6e2401b9d1c1a96fa7f6080aac78ab0b
yuriyredey <yuriyredey@gmail.com>
2022-10-27 05:13:12 +0200

    3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import Cart from './components/Cart';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
+const arr = [
+  {title: 'Nike Blazer Mid Suede', price: 129.99, imageUrl: '/img/sneakers/1.jpg'},
+  {title: 'Nike Air Max 270', price: 179.99, imageUrl: '/img/sneakers/2.jpg'},
+  {title: 'Nike Mid Blazer Suede', price: 129.99, imageUrl: '/img/sneakers/3.jpg'},
+  {title: 'Puma X Aka Boku Future Rider', price: 119.99, imageUrl: '/img/sneakers/4.jpg'},
+];
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -16,59 +23,14 @@ function App() {
         </div>
        </div>
        <div className="d-flex">
-        <Cart />
-        <div className="card">
-          <img width={133} height={112} src="/img/sneakers/2.jpg" alt="Sneakers"/>
-          <h5>Nike Air Max 270</h5>
-          <div className="d-flex justify-betwen align-center">
-            <div className="d-flex flex-column">
-              <span>Price:</span>
-              <b>179.99$</b>
-            </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-            </button>
-          </div>
-        </div>
-        <div className="card">
-          <img width={133} height={112} src="/img/sneakers/3.jpg" alt="Sneakers"/>
-          <h5>Nike Blazer Mid Suede</h5>
-          <div className="d-flex justify-betwen align-center">
-            <div className="d-flex flex-column">
-              <span>Price:</span>
-              <b>189.99$</b>
-            </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-            </button>
-          </div>
-        </div>
-        <div className="card">
-          <img width={133} height={112} src="/img/sneakers/4.jpg" alt="Sneakers"/>
-          <h5>Puma X Aka Boku Future Rider</h5>
-          <div className="d-flex justify-betwen align-center">
-            <div className="d-flex flex-column">
-              <span>Price:</span>
-              <b>139.99$</b>
-            </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-            </button>
-          </div>
-        </div>
-        <div className="card">
-          <img width={133} height={112} src="/img/sneakers/5.jpg" alt="Sneakers"/>
-          <h5>Under Armor Curry B</h5>
-          <div className="d-flex justify-betwen align-center">
-            <div className="d-flex flex-column">
-              <span>Price:</span>
-              <b>219.99$</b>
-            </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-            </button>
-          </div>
-        </div>
+        
+        {arr.map((obj) => (
+          <Cart 
+          title={obj.title}
+          price={obj.price}
+          imageUrl={obj.imageUrl}
+        />
+        ))}
        </div>
       </div>
     </div>
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,15 +1,15 @@
-function Cart() {
+function Cart(props) {
     return (
         <div className="card">
        <div className="favorite">
         <img src="/img/unliked.svg" alt="Unliked"/>
        </div>
-          <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers"/>
-          <h5>Nike Blazer Mid Suede</h5>
+          <img width={133} height={112} src={props.imageUrl} alt="Sneakers"/>
+          <h5>{props.title}</h5>
           <div className="d-flex justify-betwen align-center">
             <div className="d-flex flex-column">
               <span>Price:</span>
-              <b>189.99$</b>
+              <b>{props.price}$</b>
             </div>
             <button className="button">
               <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
